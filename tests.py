214c99644c246a86bbfb97d91e4ffe4f939b416e
GiovaniFZ <giovanifinazzi@gmail.com>
2024-11-03 15:10:41 -0300

    test: Create test suit with 2 tests

---
+++ tests.py
@@ -0,0 +1,24 @@
+import unittest
+from manager import Manage
+
+class TestClass(unittest.TestCase):
+    
+    # Positive tests
+    def test_correct_age(self):
+        name = 'Giovani'
+        age = 12
+        register = 123
+        value = 12.50
+        manager = Manage(name, age, register, value)
+        self.assertEqual(manager.checkAge(), 12)
+        
+    def test_name(self):
+        name = 'Giovani'
+        age = 12
+        register = 123
+        value = 12.50
+        manager = Manage(name, age, register, value)
+        self.assertEqual(manager.checkName(), 'Giovani')
+
+if __name__ == '__main__':
+    unittest.main()
